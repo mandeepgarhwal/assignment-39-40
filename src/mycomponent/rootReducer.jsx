9b146f33a81fb4a6d672d5a1b2909e0794bb7b63
mandeepgarhwal <114212567+mandeepgarhwal@users.noreply.github.com>
2023-08-11 18:21:15 +0530

    Update rootReducer.jsx

--- src/mycomponent/rootReducer.jsx
+++ src/mycomponent/rootReducer.jsx
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux'
 import { curdReducer } from './dairyshop/curdreducer'
 import milkReducer from './dairyshop/milkreducer'
-import {counterReducer} from './counterapp/counterreducer'
 // import { addcurdReducer } from './dairyshop/curdreducer'
 const dairyReducer = combineReducers({
         curd : curdReducer, 
         milk: milkReducer,
-        quecounter : counterReducer
-        // curdadd : addcurdReducer
+
         
 })
 
